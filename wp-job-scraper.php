d705ad076916e3dbe96424e25bef1d4d061be267
Wajih tagourty <wajih.tagourty@gmail.com>
2019-07-02 13:45:29 +0200

    version 0.1.1 hotfix

--- wp-job-scraper.php
+++ wp-job-scraper.php
@@ -35,7 +35,7 @@ if (!defined('WPINC')) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define('WP_JOB_SCRAPER_VERSION', '0.1.0');
+define('WP_JOB_SCRAPER_VERSION', '0.1.1');
 
 define('WP_JOB_SCRAPER_PATH', plugin_dir_path(__FILE__));
 
